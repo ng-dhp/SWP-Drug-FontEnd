25c181e1951ac335a0fe6d375f668e3a492b52ca
Huynh Thien Phat <huynhthienphat22@gmail.com>
2025-06-16 22:07:20 +0700

    sua app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import KhaoSat from "./page/Khaosat";
 import TuVan from "./page/tuvan";
 import AppContent from "./appContent";
 import KhoaHoc from "./page/Khoahoc.jsx"; // ✅ import file mới
+import ChienDich from "./page/ChienDich";
+
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppContent />} />
-        <Route path="/khaosat/*" element={<KhaoSat />} />
+      <Route path="/" element={<AppContent />} />
+        <Route path="/khaosat" element={<KhaoSat />} />
         <Route path="/tuvan" element={<TuVan />} />
-
+        <Route path="/chiendich" element={<ChienDich />} />
 
         <Route path="/khoahoc" element={<KhoaHoc />} />
 
